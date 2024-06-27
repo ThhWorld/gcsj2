f5269bba785200513d893c28000ba0c75206d60c
THH <tanghuanhun@163.com>
2024-06-27 11:44:05 +0800

    对管理员控制层进行了些修正

--- src/main/java/com/file_manager/Controller/AdminController.java
+++ src/main/java/com/file_manager/Controller/AdminController.java
@@ -1,15 +1,13 @@
 package com.file_manager.Controller;
 
-import com.file_manager.mapper.AdminMapper;
 import com.file_manager.pojo.File;
 import com.file_manager.service.AdminService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class AdminController {
 
     @GetMapping("/admin/file")
