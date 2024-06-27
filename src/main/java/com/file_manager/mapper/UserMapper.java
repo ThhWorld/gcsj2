d56e08d2caea12641752a797e18c1471451057c4
onepiecem <xinghaiman156@gmail.com>
2024-06-27 23:20:20 +0800

    用户控制层

--- src/main/java/com/file_manager/mapper/UserMapper.java
+++ src/main/java/com/file_manager/mapper/UserMapper.java
@@ -8,26 +8,26 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
 
-        @Select("SELECT * FROM users WHERE account = #{account} AND password = #{password}")
+        @Select("SELECT * FROM user WHERE account = #{account} AND password = #{password}")
         User findByAccountAndPassword(String account, String password);
 
-        @Select("SELECT * FROM users WHERE account = #{account}")
+        @Select("SELECT * FROM user WHERE account = #{account}")
         User findByAccount(String account);
 
-        @Select("SELECT * FROM users WHERE id = #{id}")
+        @Select("SELECT * FROM user WHERE id = #{id}")
         User findById(int id);
 
-        @Insert("INSERT INTO users(account, password, userName, phoneNumber, email, sex) VALUES(#{account}, #{password}, #{userName}, #{phoneNumber}, #{email}, #{sex})")
+        @Insert("INSERT INTO user (account, password, userName, phoneNumber, email, sex) VALUES(#{account}, #{password}, #{userName}, #{phoneNumber}, #{email}, #{sex})")
         @Options(useGeneratedKeys = true, keyProperty = "id")
         void insertUser(User user);
 
-        @Update("UPDATE users SET userName = #{userName}, phoneNumber = #{phoneNumber}, email = #{email} WHERE id = #{id}")
+        @Update("UPDATE user SET userName = #{userName}, phoneNumber = #{phoneNumber}, email = #{email} WHERE id = #{id}")
         void updateUser(User user);
 
-        @Update("UPDATE users SET password = #{password} WHERE id = #{id}")
+        @Update("UPDATE user SET password = #{password} WHERE id = #{id}")
         void updatePassword( int id, String password);
 
-        @Delete("DELETE FROM users WHERE id = #{id}")
+        @Delete("DELETE FROM user WHERE id = #{id}")
         void deleteUser(int id);
     }
 
