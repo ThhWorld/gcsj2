3f839890ab2208afdd45829807fdb3ef4dc854b4
wcl <3262028625@qq.com>
2024-06-28 22:40:30 +0800

    wcl

--- src/main/java/com/file_manager/mapper/FIleMapper.java
+++ src/main/java/com/file_manager/mapper/FIleMapper.java
@@ -1,9 +1,11 @@
 package com.file_manager.mapper;
 
+import com.file_manager.pojo.SharedFile;
 import org.apache.ibatis.annotations.*;
 
 import org.springframework.stereotype.Controller;
 import com.file_manager.pojo.File;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -26,4 +28,20 @@ public interface FIleMapper {
 
     @Select("SELECT * from shared_file where id=#{id}")
     File SelectShare(int id);
+
+    @Update("update file set file_name=#{fileName} where id=#{id}")
+    int reName(@Param("id")int id,@Param("fileName") String fileName);
+
+    @Select("select * from file where use_id=#{useid}")
+    List<File>  rearchFile(@Param("useid") int useid);
+
+    @Select("select * from shared_file")
+    List<SharedFile> rearchSharedFile();
+    @Update("update file set is_deleted=if(is_deleted=0,1,0) where id=#{id}")
+    int toBin(int id);
+    @Delete("delete from file where id=#{id}")
+    int deleteFile(int id);
+    @Select("select local_path from file where id=#{id}")
+    String findLocalPath(int id);
+
 }
