ca7ecffb611d1db88538c5fcce4dfe0baeccdcd9
tinyrole <908448485@qq.com>
2024-06-30 11:30:22 +0800

    tinyrole

--- qd/src/router/index.js
+++ qd/src/router/index.js
@@ -1,17 +1,50 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/recallmanage',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/RecallView.vue')
+    },
+    {
+      path: '/recall',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/RecallView.vue')
+    },
+    {
+      path: '/sharemanage',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ShareManageView.vue')
+    },
+    {
+      path: '/share',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ShareView.vue')
+    },
+    {
+      path: '/usermanage',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/UserManageView.vue')
+    }
+  ]
 })
+
+export default router
