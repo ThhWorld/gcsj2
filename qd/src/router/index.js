a9ef3e7e9e441459240ad91a9797a358a65c5dc9
xl <2302772951@qq.com>
2024-06-30 12:49:17 +0800

    编写用户信息修改，密码修改界面

--- qd/src/router/index.js
+++ qd/src/router/index.js
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import login from '../views/Login'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: login
     },
     {
       path: '/recallmanage',
@@ -43,7 +42,13 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/UserManageView.vue')
+
+    },
+    {
+      path :'/register',
+      component: ()=>import('../views/Register')
     }
+
   ]
 })
 
