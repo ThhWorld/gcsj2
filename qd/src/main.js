bfdb6a7689d664032a6e47c23e48e64ebbdf8585
xl <2302772951@qq.com>
2024-06-30 16:45:22 +0800

    login 能用一点了

--- qd/src/main.js
+++ qd/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import ElementUI from 'element-ui';
 import axios from 'axios';
 import 'element-ui/lib/theme-chalk/index.css';
@@ -8,7 +9,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = axios
-axios.defaults.baseURL = '/api'; //后端地址
+axios.defaults.baseURL = '/'; //后端地址
 //注册插件
 Vue.use(ElementUI)
 
--- qd/src/router/index.js
+++ qd/src/router/index.js
@@ -1,70 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/Login.vue'
+import Login from '../views/user/Login.vue'
 import Register from '../views/Register.vue'
-import MainPage from "@/views/MainPage.vue";
+import Index from '../views/Index.vue'
 
+Vue.use(VueRouter)
 
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/index',
+    name: 'Index',
+    component: Index
+  },
+]
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      redirect: '/login'
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register
-    },
-    {
-      path:'/MainPage',
-      name:'MainPage',
-      component: MainPage
-    },
-    {
-      path: '/recallmanage',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/RecallView.vue')
-    },
-    {
-      path: '/recall',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/RecallView.vue')
-    },
-    {
-      path: '/sharemanage',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ShareManageView.vue')
-    },
-    {
-      path: '/share',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ShareView.vue')
-    },
-    {
-      path: '/usermanage',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/UserManageView.vue')
-    }
-  ]
+const router = new VueRouter({
+  routes
 })
 
 export default router
