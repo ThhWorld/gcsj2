eced00939a6f0d05e92fa7f4f10c52eed219160c
tinyrole <908448485@qq.com>
2024-06-30 11:30:22 +0800

    删掉了一些无用文件，修改了index中的router

--- qd/src/router/index.js
+++ qd/src/router/index.js
@@ -1,17 +1,70 @@
+import { createRouter, createWebHistory } from 'vue-router'
 import Vue from 'vue'
-import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import MainPage from "@/views/MainPage.vue";
 
-Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path:'/MainPage',
+      name:'MainPage',
+      component: MainPage
+    },
+    {
+      path: '/recallmanage',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/RecallView.vue')
+    },
+    {
+      path: '/recall',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/RecallView.vue')
+    },
+    {
+      path: '/sharemanage',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ShareManageView.vue')
+    },
+    {
+      path: '/share',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ShareView.vue')
+    },
+    {
+      path: '/usermanage',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/UserManageView.vue')
+    }
+  ]
 })
+
+export default router
