cc988ce3c6b80984aae2c5df3532e8d15f08fd5b
chaihuo-dead <ternaryspirit@163.com>
2024-06-30 10:28:25 +0800

    mainpage

---
+++ qd/src/views/adminPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ msg}}
+  </div>
+</template>
+
+<!--加上scoped能够防止样式之间的冲突-->
+<style scoped>
+body {
+  background-color: #ff0000;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {msg: '这个是Vue模板页'}
+  }
+}
+</script>
+
